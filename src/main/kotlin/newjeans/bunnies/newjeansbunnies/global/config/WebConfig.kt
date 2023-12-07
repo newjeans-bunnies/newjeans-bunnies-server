5c9f64eee12f92cb4d103bc689d46167e1a79823
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-07 10:18:23 +0900

    :zap: :: set corsMappings

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/WebConfig.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/WebConfig.kt
@@ -1,16 +1,45 @@
 package newjeans.bunnies.newjeansbunnies.global.config
 
+
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    @Value("\${cors.originPatterns}")
+    private val corsOriginPatterns: String
+) : WebMvcConfigurer {
+
+    val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
+
     override fun addCorsMappings(registry: CorsRegistry) {
         super.addCorsMappings(registry)
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET","POST","PATCH")
+
+        registry.addMapping("/api/auth/**")
+            .allowedOriginPatterns(*allowedOrigins)
+            .allowedMethods("POST","PATCH")
             .allowedHeaders("*")
+
+        registry.addMapping("/api/comment/**")
+            .allowedOriginPatterns(*allowedOrigins)
+            .allowedMethods("POST","DELETE")
+            .allowedHeaders("*")
+
+        registry.addMapping("/api/aws/image/**")
+            .allowedOriginPatterns(*allowedOrigins)
+            .allowedMethods("GET")
+            .allowedHeaders("*")
+
+        registry.addMapping("/api/post/**")
+            .allowedOriginPatterns(*allowedOrigins)
+            .allowedMethods("GET","POST")
+            .allowedHeaders("*")
+
+        registry.addMapping("/api/user/**")
+            .allowedOriginPatterns(*allowedOrigins)
+            .allowedMethods("POST")
     }
 }
