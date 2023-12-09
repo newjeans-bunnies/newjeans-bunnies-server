0f1381d7f7dd1aedc5772a5a9335f8be29e62a81
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-09 23:47:19 +0900

    :fire: :: delete UserService.kt

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/user/service/UserService.kt
+++
@@ -1,9 +0,0 @@
-package newjeans.bunnies.newjeansbunnies.domain.user.service
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Service
-
-@Service
-@Configuration
-class UserService {
-}
