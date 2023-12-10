169f0392674c940793a77bc702ac258ce3dfdc9a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-11 03:11:24 +0900

    :sparkles: :: add PostReport logic

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/report/controller/dto/request/ReportPostRequestDto.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/report/controller/dto/request/ReportPostRequestDto.kt
@@ -1,11 +1,13 @@
 package newjeans.bunnies.newjeansbunnies.domain.report.controller.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class ReportPostRequestDto(
     @field:NotBlank(message = "아이디는 필수 입력 값입니다.")
     val id: String,
     @field:NotBlank(message = "전화번호는 필수 입력 값입니다.")
+    @field:Pattern(regexp = "^(010)-([0-9]{4})-([0-9]{4})$", message = "전화번호를 적어주세요 ex) 010-1234-5678")
     val phoneNumber: String,
     @field:NotBlank(message = "게시글아이디는 필수 입력 값입니다.")
     val postId: String
--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/report/service/PostReportService.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/report/service/PostReportService.kt
@@ -1,18 +1,41 @@
 package newjeans.bunnies.newjeansbunnies.domain.report.service
 
 
+import newjeans.bunnies.newjeansbunnies.domain.auth.error.exception.NotExitstIdAndPhoneNumberException
+import newjeans.bunnies.newjeansbunnies.domain.post.error.exception.NotExistPostIdException
+import newjeans.bunnies.newjeansbunnies.domain.post.repository.PostRepository
+import newjeans.bunnies.newjeansbunnies.domain.report.PostReportEntity
 import newjeans.bunnies.newjeansbunnies.domain.report.controller.dto.request.ReportPostRequestDto
 import newjeans.bunnies.newjeansbunnies.domain.report.repository.PostReportRepository
+import newjeans.bunnies.newjeansbunnies.domain.user.repository.UserRepository
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
+import java.util.*
 
 
 @Service
 @Configuration
 class PostReportService(
-    private val postReportRepository: PostReportRepository
+    private val postReportRepository: PostReportRepository,
+    private val postRepository: PostRepository,
+    private val userRepository: UserRepository
 ) {
-    fun execute(reportPostRequestDto: ReportPostRequestDto){
+    fun execute(reportPostRequestDto: ReportPostRequestDto) {
+        userRepository.findByUserIdAndPhoneNumber(reportPostRequestDto.id, reportPostRequestDto.phoneNumber).orElseThrow {
+            throw NotExitstIdAndPhoneNumberException
+        }
 
+        postRepository.findById(reportPostRequestDto.postId).orElseThrow {
+            throw NotExistPostIdException
+        }
+
+        postReportRepository.save(
+            PostReportEntity(
+                uuid = UUID.randomUUID().toString(),
+                id = reportPostRequestDto.id,
+                phoneNumber = reportPostRequestDto.phoneNumber,
+                postId = reportPostRequestDto.postId
+            )
+        )
     }
 }
