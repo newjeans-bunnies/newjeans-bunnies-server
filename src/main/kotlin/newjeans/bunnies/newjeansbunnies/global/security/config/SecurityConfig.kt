dc7e94c94ef37eab18a5e01aedb53fa986edc59e
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-11 03:09:59 +0900

    :zap: :: update SecurityConfig

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/security/config/SecurityConfig.kt
@@ -32,37 +32,51 @@ class SecurityConfig(
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf { csrf -> csrf.disable() }
             .authorizeHttpRequests { authorize ->
+
                 //auth
                 authorize.requestMatchers("/api/auth/**").permitAll()
+
 
                 //post
                 authorize.requestMatchers(HttpMethod.POST, "/api/post/good").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
                 authorize.requestMatchers(HttpMethod.POST, "/api/post").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
-                authorize.requestMatchers(HttpMethod.GET, "/api/post/**").permitAll()
+                authorize.requestMatchers(HttpMethod.GET, "/api/post/detail/**").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers(HttpMethod.GET, "/api/post/basic-info/**").permitAll()
 
-                //comment
-                authorize.requestMatchers(HttpMethod.POST,"/api/comment/**").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
-                authorize.requestMatchers(HttpMethod.DELETE,"/api/comment/**").hasAuthority(Authority.MANAGER.name)
-                authorize.requestMatchers(HttpMethod.GET,"/api/comment/**").permitAll()
+
+                //parentsComment
+                authorize.requestMatchers(HttpMethod.POST,"/api/comment/parents/send").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers(HttpMethod.GET,"/api/comment/parents/detail").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers(HttpMethod.GET,"/api/comment/parents/basic-info").permitAll()
+                authorize.requestMatchers(HttpMethod.DELETE,"/api/comment/parents").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+
+
+                //childrenComment
+                authorize.requestMatchers(HttpMethod.POST,"/api/comment/children/send").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers(HttpMethod.GET,"/api/comment/children/detail").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers(HttpMethod.GET,"/api/comment/children/basic-info").permitAll()
+                authorize.requestMatchers(HttpMethod.DELETE,"/api/comment/children").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
 
 
                 //image
                 authorize.requestMatchers("/api/aws/image/post").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
                 authorize.requestMatchers("/api/aws/image/user").permitAll()
 
+
                 //good
                 authorize.requestMatchers("/api/post/good").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers("/api/comment/children/good").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+                authorize.requestMatchers("/api/comment/parents/good").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
+
 
                 //user
                 authorize.requestMatchers(HttpMethod.GET,"/api/user/get-detail/**").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
                 authorize.requestMatchers(HttpMethod.GET,"api/user/get-basic/**").permitAll()
                 authorize.requestMatchers(HttpMethod.PATCH,"/api/user/update").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
 
-                //report
-                authorize.requestMatchers(HttpMethod.POST,"/api/report/post").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
-                authorize.requestMatchers(HttpMethod.POST,"/api/report/parents-comment").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
-                authorize.requestMatchers(HttpMethod.POST,"/api/report/children-comment").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
 
+                //report
+                authorize.requestMatchers(HttpMethod.POST,"/api/report/**").hasAnyAuthority(Authority.USER.name, Authority.MANAGER.name)
 
             }
             .exceptionHandling { exceptionHandling ->
