7c4e285f7eec24f8c4f51f2dc7f2d485f2801bda
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-05 17:22:15 +0900

    :bug: :: fix redis connection

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/service/PhoneNumberCertificationService.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/service/PhoneNumberCertificationService.kt
@@ -1,13 +1,13 @@
 package newjeans.bunnies.newjeansbunnies.domain.auth.service
 
-import newjeans.bunnies.newjeansbunnies.domain.auth.CertificationNumberEntity
 import newjeans.bunnies.newjeansbunnies.domain.auth.controller.dto.request.CertificationVerifyRequestDto
 import newjeans.bunnies.newjeansbunnies.domain.auth.controller.dto.response.CertificationVerifyResponseDto
 import newjeans.bunnies.newjeansbunnies.domain.auth.error.exception.FailedAuthenticationException
-import newjeans.bunnies.newjeansbunnies.domain.auth.repository.CertificationNumberRepository
+import newjeans.bunnies.newjeansbunnies.global.config.RedisConfig
 import newjeans.bunnies.newjeansbunnies.global.utils.SmsUtil
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
+import java.time.Duration
 import kotlin.random.Random
 import kotlin.random.nextInt
 
@@ -15,27 +15,32 @@ import kotlin.random.nextInt
 @Configuration
 class PhoneNumberCertificationService(
     private val smsUtil: SmsUtil,
-    private val phoneNumberNumberRepository: CertificationNumberRepository
+    private val redisConfig: RedisConfig
 ) {
     fun verify(certificationVerifyRequestDto: CertificationVerifyRequestDto): CertificationVerifyResponseDto {
-        phoneNumberNumberRepository.findByPhoneNumberAndCertificationNumber(
-            certificationVerifyRequestDto.phoneNumber,
-            certificationVerifyRequestDto.certificationNumber
-        ).orElseThrow {
+        if(getValues(certificationVerifyRequestDto.phoneNumber) != certificationVerifyRequestDto.certificationNumber)
             throw FailedAuthenticationException
-        }
         return CertificationVerifyResponseDto(200, "success")
     }
 
     fun certification(phoneNumber: String): CertificationVerifyResponseDto {
         val random = Random.Default
         var randomNumber = ""
-        for (i in 0..3) {
+        for (i in 0..5) {
             randomNumber += random.nextInt(0..9).toString()
         }
-        val certificationNumberEntity = CertificationNumberEntity(phoneNumber, randomNumber)
-        phoneNumberNumberRepository.save(certificationNumberEntity)
-        smsUtil.sendOne(phoneNumber, randomNumber)
+        setValues(phoneNumber, randomNumber)
+        smsUtil.sendOne(phoneNumber, "[인증번호: $randomNumber] NewJeans-Bunnies 인증번호입니다.")
         return CertificationVerifyResponseDto(200, "success")
+    }
+
+    private fun setValues(phoneNumber: String, randomNumber: String){
+        val values = redisConfig.redisTemplate().opsForValue()
+        values.set(phoneNumber, randomNumber, Duration.ofMinutes(1))
+    }
+
+    private fun getValues(phoneNumber: String): String? {
+        val value = redisConfig.redisTemplate().opsForValue()
+        return value.get(phoneNumber)
     }
 }
--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/RedisConfig.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -25,8 +26,7 @@ class RedisConfig(
         val redisStandaloneConfiguration = RedisStandaloneConfiguration()
         redisStandaloneConfiguration.hostName = host
         redisStandaloneConfiguration.port = port
-        redisStandaloneConfiguration.setPassword(password)
-        redisStandaloneConfiguration.username = "HamTory"
+        redisStandaloneConfiguration.password = RedisPassword.of(password)
 
         val lettuceConnectionFactory = LettuceConnectionFactory(redisStandaloneConfiguration)
         return lettuceConnectionFactory
