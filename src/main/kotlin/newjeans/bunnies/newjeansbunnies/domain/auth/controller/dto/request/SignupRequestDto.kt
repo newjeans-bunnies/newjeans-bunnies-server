8dcebc1ba068d9b7b2bc100d9efa975c388bb9c9
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-13 23:28:44 +0900

    :lock: :: fix password Pattern

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/controller/dto/request/SignupRequestDto.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/controller/dto/request/SignupRequestDto.kt
@@ -10,7 +10,7 @@ data class SignupRequestDto(
     @field:Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "아이디는 4~12자 영문 대 소문자, 숫자만 사용하세요.")
     val userId: String,
     @field:NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @field:Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()])(?=\\S+\$).{8,16}\$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\d)(?=.*[~․!@#\$%^&*()_\\-+=|\\\\;:‘“<>,.?/]).{8,16}\$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     val password: String,
     @field:NotBlank(message = "전화번호는 필수 입력 값입니다.")
     @field:Pattern(regexp = "^(010)([0-9]{4})([0-9]{4})$", message = "전화번호를 적어주세요 ex) 010-1234-5678")
