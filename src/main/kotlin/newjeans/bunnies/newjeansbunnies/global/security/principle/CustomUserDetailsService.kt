c81d39d2a8ae885cecaf3fe37252a58c4474f415
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-03-18 11:19:41 +0900

    :passport_control: :: fix JPA

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/security/principle/CustomUserDetailsService.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/security/principle/CustomUserDetailsService.kt
@@ -3,6 +3,7 @@ package newjeans.bunnies.newjeansbunnies.global.security.principle
 
 import newjeans.bunnies.newjeansbunnies.domain.user.repository.UserRepository
 import newjeans.bunnies.newjeansbunnies.global.error.exception.InvalidTokenException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
@@ -13,7 +14,7 @@ class CustomUserDetailsService(
     private val userRepository: UserRepository
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
-        val user = userRepository.findByUserId(username).orElseThrow{throw InvalidTokenException}
+        val user = userRepository.findByIdOrNull(username) ?: throw InvalidTokenException
         return CustomUserDetails(user.userId)
     }
 
