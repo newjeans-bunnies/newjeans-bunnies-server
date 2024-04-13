a8a9ba8d9a770a75aea1d97dd18c086a6bfbc6fb
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-04-14 00:50:23 +0900

    :adhesive_bandage: :: Fix Password error Rest API method change

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/error/AuthErrorCode.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/auth/error/AuthErrorCode.kt
@@ -10,7 +10,7 @@ enum class AuthErrorCode(
 ) : CustomErrorProperty {
 
     EXIST_USER_ID_EXCEPTION(409, "아이디가 이미 존재합니다."),
-    INVALID_PASSWORD_EXCEPTION(401,"유효하지 않는 비밀번호입니다."),
+    INVALID_PASSWORD_EXCEPTION(409,"유효하지 않는 비밀번호입니다."),
     NOT_EXIST_ID(409, "존재하지 않는 아이디입니다."),
     EXIST_PHONE_NUMBER_EXCEPTION(409, "이미 등록된 전화번호입니다."),
     COUNTRY_NOT_FOUND_EXCEPTION(400, "지원하지 않거나 존재하지 않는 나라입니다."),//language
