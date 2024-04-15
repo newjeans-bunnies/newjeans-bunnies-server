daaf6c3af377092dd49d03655c42c4df5b597c03
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-04-15 20:41:09 +0900

    :zap: :: aws s3 PreSignedUrl upload bug fix and logic fix

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/image/controller/dto/request/PreSignedUrlCreateRequest.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/image/controller/dto/request/PreSignedUrlCreateRequest.kt
@@ -3,4 +3,7 @@ package newjeans.bunnies.newjeansbunnies.domain.image.controller.dto.request
 data class PreSignedUrlCreateRequest(
     val uploadId: String,
     val partNumber: Int,
+    val fileType: String,
+    val fileName: String,
+    val zoneId: String
 )
