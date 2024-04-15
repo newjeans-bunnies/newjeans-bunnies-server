967895b80cf27b8995877d98d005643b3bb7aa77
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-04-15 20:45:02 +0900

    :technologist: :: Post deactivation logic

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/post/error/PostErrorCode.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/post/error/PostErrorCode.kt
@@ -10,10 +10,7 @@ enum class PostErrorCode(
 ) : CustomErrorProperty {
     NOT_EXIST_POST_ID(409, "존재하지 않는 게시글아이디 입니다."),
     OVER_FILE(400, "사진 최대 갯수는 10개 입니다."),
-    NOT_EXIST_POST_IMAGE(200, "게시글에 사진이 존재하지 않습니다"),
-    DISABLED_POST(200, "삭제된 게시글입니다")
-
-    ;
+    DISABLED_POST(200, "삭제된 게시글입니다");
 
     override fun status(): Int = status
     override fun message(): String = message
