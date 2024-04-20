464efce8f4b8d124596b11e97a1013016a093409
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-04-20 22:33:13 +0900

    :zap: :: fix CreatePreSignedUrlResponse dto

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/image/controller/dto/response/CreatePreSignedUrlResponse.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/domain/image/controller/dto/response/CreatePreSignedUrlResponse.kt
@@ -3,5 +3,7 @@ package newjeans.bunnies.newjeansbunnies.domain.image.controller.dto.response
 data class CreatePreSignedUrlResponse(
     val preSignedUrl: String,
     val fileName: String,
-    val postId: String
+    val postId: String,
+    val fileSize: String,
+    val fileType: String
 )
