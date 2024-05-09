cb6ebe41a43721c7e8ca342f771ccf3b98250297
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-05-09 12:09:40 +0900

    :ambulance: :: set Access-Control-Allow-Credentials true

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/WebConfig.kt
+++ src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/config/WebConfig.kt
@@ -1,12 +1,9 @@
 package newjeans.bunnies.newjeansbunnies.global.config
 
-
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 
 @Configuration
 class WebConfig(
@@ -21,37 +18,43 @@ class WebConfig(
         //auth
         registry.addMapping("/api/auth/**")
             .allowedOrigins(allowedOrigins)
-            .allowedMethods("POST","PATCH")
+            .allowedMethods("POST","PATCH", "OPTIONS")
             .allowedHeaders("*")
+            .allowCredentials(true)
 
         //comment
         registry.addMapping("/api/comment/**")
             .allowedOrigins(allowedOrigins)
             .allowedMethods("POST","DELETE")
             .allowedHeaders("*")
+            .allowCredentials(true)
 
         //image
         registry.addMapping("/api/image/**")
             .allowedOrigins(allowedOrigins)
             .allowedMethods("GET")
             .allowedHeaders("*")
+            .allowCredentials(true)
 
         //post
         registry.addMapping("/api/post/**")
             .allowedOrigins(allowedOrigins)
             .allowedMethods("GET","POST","DELETE")
             .allowedHeaders("*")
+            .allowCredentials(true)
 
         //user
         registry.addMapping("/api/user/**")
             .allowedOrigins(allowedOrigins)
             .allowedMethods("GET","PATCH","DELETE")
             .allowedHeaders("*")
+            .allowCredentials(true)
 
         //report
         registry.addMapping("/api/report/**")
             .allowedOrigins(allowedOrigins)
             .allowedMethods("POST")
             .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
