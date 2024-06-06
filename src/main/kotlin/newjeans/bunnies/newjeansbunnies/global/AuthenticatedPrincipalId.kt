97faec3b94356e11e2b6e7f746d97347bf69ca1a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-06-06 18:44:48 +0900

    :fire: :: remove code

--- src/main/kotlin/newjeans/bunnies/newjeansbunnies/global/AuthenticatedPrincipalId.kt
+++
@@ -1,8 +0,0 @@
-package newjeans.bunnies.newjeansbunnies.global
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : #this.getUsername()")
-annotation class AuthenticatedPrincipalId
